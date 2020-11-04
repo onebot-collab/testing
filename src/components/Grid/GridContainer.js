2f01034bfed66235c96707a40c6bdfe1dd6c3de7
edo <edo@gmi.co>
2020-11-05 04:50:31 +0700

    Add User

--- src/components/Grid/GridContainer.js
+++ src/components/Grid/GridContainer.js
@@ -2,23 +2,24 @@ import React from 'react'
 // nodejs library to set properties for components
 import PropTypes from 'prop-types'
 // @material-ui/core components
-import { makeStyles } from '@material-ui/core/styles'
+// import { makeStyles } from '@material-ui/core/styles'
 import Grid from '@material-ui/core/Grid'
+import '../../pages/User/Actual.css'
 
-const styles = {
-  grid: {
-    margin: '0 -15px !important',
-    width: 'unset',
-  },
-}
+// const styles = {
+//   grid: {
+//     margin: '0 -15px !important',
+//     width: 'unset',
+//   },
+// }
 
-const useStyles = makeStyles(styles)
+// const useStyles = makeStyles(styles)
 
 export default function GridContainer(props) {
-  const classes = useStyles()
+  // const classes = useStyles()
   const { children, ...rest } = props
   return (
-    <Grid container {...rest} className={classes.grid}>
+    <Grid container {...rest} className="fieldGridContainer">
       {children}
     </Grid>
   )
--- src/pages/User/Actual.js
+++ src/pages/User/Actual.js
@@ -1,7 +1,7 @@
+/* eslint-disable react/no-access-state-in-setstate */
 import React, { Component } from 'react'
 import './Actual.css'
 // @material-ui/core components
-import InputLabel from '@material-ui/core/InputLabel'
 import List from '@material-ui/core/List'
 import ListItem from '@material-ui/core/ListItem'
 import ListItemAvatar from '@material-ui/core/ListItemAvatar'
@@ -9,13 +9,14 @@ import ListItemSecondaryAction from '@material-ui/core/ListItemSecondaryAction'
 import ListItemText from '@material-ui/core/ListItemText'
 import Avatar from '@material-ui/core/Avatar'
 import Grid from '@material-ui/core/Grid'
+import TextField from '@material-ui/core/TextField'
+import MenuItem from '@material-ui/core/MenuItem'
 import TablePagination from '@material-ui/core/TablePagination'
 // @material-ui/icons components
 import Visibility from '@material-ui/icons/Visibility'
 // core components
 import GridItem from '../../components/Grid/GridItem'
 import GridContainer from '../../components/Grid/GridContainer'
-import CustomInput from '../../components/CustomInput/CustomInput'
 import Button from '../../components/CustomButtons/Button'
 import Card from '../../components/Card/Card'
 import CardHeader from '../../components/Card/CardHeader'
@@ -25,6 +26,30 @@ import CardFooter from '../../components/Card/CardFooter'
 import avatar from '../../assets/img/faces/marc.jpg'
 
 export default class User extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      name: '',
+      email: '',
+      phone: '',
+      password: '',
+      passcode: '',
+      joinedDate: '',
+      birthDate: '',
+      address: '',
+      role: '',
+      department: '',
+      timeType: '',
+      profilePicture: null,
+    }
+    this.handleChange = this.handleChange.bind(this)
+  }
+
+  handleChange(event) {
+    this.setState({ [event.target.name]: event.target.value })
+    console.log('ROLE SELECTED', event.target.value)
+  }
+
   render() {
     return (
       <div>
@@ -36,102 +61,152 @@ export default class User extends Component {
                 <p className="cardCategoryWhite">by Admin</p>
               </CardHeader>
               <CardBody>
-                <GridContainer>
-                  <GridItem xs={12} sm={12} md={5}>
-                    <CustomInput
-                      labelText="Company (disabled)"
-                      id="company-disabled"
-                      formControlProps={{
-                        fullWidth: true,
-                      }}
-                      inputProps={{
-                        disabled: true,
-                      }}
-                    />
-                  </GridItem>
-                  <GridItem xs={12} sm={12} md={3}>
-                    <CustomInput
-                      labelText="Username"
-                      id="username"
-                      formControlProps={{
-                        fullWidth: true,
-                      }}
+                <GridContainer className="fieldGridContainer">
+                  <GridItem xs={12} sm={12} md={4}>
+                    <TextField
+                      label="Name"
+                      name="name"
+                      value={this.state.name}
+                      onChange={this.handleChange}
+                      className="textFieldWidth"
                     />
                   </GridItem>
                   <GridItem xs={12} sm={12} md={4}>
-                    <CustomInput
-                      labelText="Email address"
-                      id="email-address"
-                      formControlProps={{
-                        fullWidth: true,
-                      }}
+                    <TextField
+                      label="Email"
+                      type="email"
+                      name="email"
+                      value={this.state.email}
+                      onChange={this.handleChange}
+                      className="textFieldWidth"
+                    />
+                  </GridItem>
+                  <GridItem xs={12} sm={12} md={4}>
+                    <TextField
+                      label="Phone"
+                      type="phone"
+                      name="phone"
+                      value={this.state.phone}
+                      onChange={this.handleChange}
+                      className="textFieldWidth"
                     />
                   </GridItem>
                 </GridContainer>
                 <GridContainer>
                   <GridItem xs={12} sm={12} md={6}>
-                    <CustomInput
-                      labelText="First Name"
-                      id="first-name"
-                      formControlProps={{
-                        fullWidth: true,
-                      }}
+                    <TextField
+                      label="Password"
+                      type="password"
+                      name="password"
+                      value={this.state.password}
+                      onChange={this.handleChange}
+                      className="textFieldWidth"
                     />
                   </GridItem>
                   <GridItem xs={12} sm={12} md={6}>
-                    <CustomInput
-                      labelText="Last Name"
-                      id="last-name"
-                      formControlProps={{
-                        fullWidth: true,
-                      }}
+                    <TextField
+                      label="Passcode"
+                      type="password"
+                      name="passcode"
+                      value={this.state.passcode}
+                      onChange={this.handleChange}
+                      className="textFieldWidth"
                     />
                   </GridItem>
                 </GridContainer>
                 <GridContainer>
                   <GridItem xs={12} sm={12} md={4}>
-                    <CustomInput
-                      labelText="City"
-                      id="city"
-                      formControlProps={{
-                        fullWidth: true,
-                      }}
+                    <TextField
+                      label="Role"
+                      name="role"
+                      value={this.state.role}
+                      onChange={this.handleChange}
+                      className="textFieldWidth"
+                      select
+                    >
+                      <MenuItem key={1} value={1}>
+                        Admin
+                      </MenuItem>
+                      <MenuItem key={2} value={2}>
+                        User
+                      </MenuItem>
+                    </TextField>
+                  </GridItem>
+                  <GridItem xs={12} sm={12} md={4}>
+                    <TextField
+                      label="Department"
+                      name="department"
+                      value={this.state.department}
+                      onChange={this.handleChange}
+                      className="textFieldWidth"
+                      select
+                    >
+                      <MenuItem key={1} value={1}>
+                        General
+                      </MenuItem>
+                      <MenuItem key={2} value={2}>
+                        Development
+                      </MenuItem>
+                    </TextField>
+                  </GridItem>
+                  <GridItem xs={12} sm={12} md={4}>
+                    <TextField
+                      label="Time type"
+                      name="timeType"
+                      value={this.state.timeType}
+                      onSelect={this.handleChange}
+                      className="textFieldWidth"
+                      select
+                    >
+                      <MenuItem key={1} value={1}>
+                        Office hours
+                      </MenuItem>
+                      <MenuItem key={2} value={2}>
+                        Free hours
+                      </MenuItem>
+                    </TextField>
+                  </GridItem>
+                </GridContainer>
+                <GridContainer>
+                  <GridItem xs={12} sm={12} md={4}>
+                    <TextField
+                      label="Joined date"
+                      type="date"
+                      name="joinedDate"
+                      value={this.state.joinedDate}
+                      onChange={this.handleChange}
+                      className="textFieldWidth"
                     />
                   </GridItem>
                   <GridItem xs={12} sm={12} md={4}>
-                    <CustomInput
-                      labelText="Country"
-                      id="country"
-                      formControlProps={{
-                        fullWidth: true,
-                      }}
+                    <TextField
+                      label="Birth date"
+                      type="date"
+                      name="birthDate"
+                      value={this.state.birthDate}
+                      onChange={this.handleChange}
+                      className="textFieldWidth"
                     />
                   </GridItem>
                   <GridItem xs={12} sm={12} md={4}>
-                    <CustomInput
-                      labelText="Postal Code"
-                      id="postal-code"
-                      formControlProps={{
-                        fullWidth: true,
-                      }}
+                    <TextField
+                      label="Address"
+                      name="address"
+                      value={this.state.address}
+                      onChange={this.handleChange}
+                      className="textFieldWidth"
                     />
                   </GridItem>
                 </GridContainer>
                 <GridContainer>
                   <GridItem xs={12} sm={12} md={12}>
-                    <InputLabel style={{ color: '#AAAAAA' }}>
-                      About me
-                    </InputLabel>
-                    <CustomInput
-                      labelText="Lamborghini Mercy, Your chick she so thirsty, I'm in that two seat Lambo."
-                      id="about-me"
-                      formControlProps={{
-                        fullWidth: true,
-                      }}
-                      inputProps={{
-                        multiline: true,
-                        rows: 5,
-                      }}
+                    <TextField
+                      label="Profile Picture"
+                      type="file"
+                      name="profilePicture"
+                      value={this.state.profilePicture}
+                      onChange={this.handleChange}
+                      className="textFieldWidth"
                     />
                   </GridItem>
                 </GridContainer>
