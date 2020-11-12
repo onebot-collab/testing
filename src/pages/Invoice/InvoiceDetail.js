8d5ae763492238cd2dc2f6ad8f6f8cd0ef7e089d
it.dev <ilham@gmi.co>
2020-11-12 22:37:49 +0700

    invoice detail

--- src/pages/Invoice/InvoiceDetail.js
+++ src/pages/Invoice/InvoiceDetail.js
@@ -62,8 +62,12 @@ export default class InvoiceDetail extends Component {
           <GridItem xs={12} sm={12} md={12}>
             <Card>
               <CardHeader color="danger">
-                <h4 className={classes.cardTitleWhite}>01-GMI-2020</h4>
-                <p className={classes.cardCategoryWhite}>by Charles</p>
+                <h4 className={classes.cardTitleWhite}>
+                  {this.props.location.state.invoice_no}
+                </h4>
+                <p className={classes.cardCategoryWhite}>
+                  by {this.props.location.state.requestname}
+                </p>
               </CardHeader>
               <CardBody>
                 <Grid item xs container direction="column" spacing={2}>
@@ -75,7 +79,7 @@ export default class InvoiceDetail extends Component {
                     </ListItem>
 
                     <Typography variant="body2" className="textPrimaryColor">
-                      Lorem ipsum
+                      {this.props.location.state.note}
                     </Typography>
                   </Grid>
                   <Grid item xs>
